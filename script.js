520f70ee425cbc820d4174bb2a78fa7d2e80d2ff
BuggyFord <128647975+BuggyFord@users.noreply.github.com>
2023-05-15 13:58:35 -0500

    added more code to javascript to make the existing code more functional according to user story.

--- script.js
+++ script.js
@@ -10,6 +10,12 @@ var secondButt = document.getElementById("button2")
 var thirdButt = document.getElementById("button3")
 var fourthButt = document.getElementById("button4")
 var questionScreen = document.querySelector('.question-screen');
+var rightWrongContainer = document.getElementById('check');
+var endScreen = document.querySelector('.quiz-finish-screen');
+var submitButton = document.getElementById('initials-button');
+var formInitials = document.getElementById('initials-form');
+
+
 var questionIndex = 0; 
 var questions = [
     {
@@ -58,8 +64,10 @@ function initial(){
     renderQuiz();
 }
 
+var interval;
+
 function timerRun(){
-    var interval = setInterval(function(){
+    interval = setInterval(function(){
         timeLeftEl.textContent = timerLeft;
         timerLeft--;
         if(timerLeft < 0){
@@ -70,6 +78,8 @@ function timerRun(){
 }
 
 function renderQuiz(){
+    console.log("Current Question Index: ", questionIndex);
+
     quizTitle.textContent= questions[questionIndex].title
     firstButt.textContent= questions[questionIndex].options[0]
     secondButt.textContent= questions[questionIndex].options[1]
@@ -87,14 +97,64 @@ function rightWrong (event){
     console.log(event.target.textContent)
     console.log(questions[questionIndex].answer)
     */
-    if(questions[questionIndex].answer == event.trigger.textContent){
-        console.log("CORRECT")
+    if(questions[questionIndex].answer == event.target.textContent){
+        console.log("CORRECT");
+        rightWrongContainer.textContent = "CORRECT";
+        // increment the users score
+       // score += 20;
+
     }else {
         console.log("Wrong")
+        rightWrongContainer.textContent = "Wrong";
+        // timerLeft = timeLeftEl - 5;
+    }
+
+    // and then what? 
+
+    // DO WE need to Clear the screen (*IMPORTANT*) 
+
+    // We want to show the NEXT question
+    // Update our Current Question INDEX value
+    questionIndex = questionIndex + 1;
+    // TEST --> Do we have more questions?
+    if(questionIndex < questions.length) {
+        // YES --> show next question
+        renderQuiz();
+
+    } else {
+        // NO --> go to next section (User initails entered)
+        endQuiz();
     }
 
 }
 
+
+function endQuiz() {
+    console.log("End of Questions...")
+
+    // what logic happens here
+
+    // we want to stop TIMER
+        // we might want to grab the timeLeft and use that for the USER SCORE(?)
+    clearInterval(interval)
+
+    // we show the FINAL QUIZ SCREEN
+        // we should HIDE the questions container
+    questionScreen.style.display="none"
+        //  we need to UNHIDE the quiz-finish-screen section
+    endScreen.style.display="block";
+}
+
+
+// submitButton.addEventListener('click', function(event) { // callback logic })
+
+submitButton.addEventListener('click', captureInitals);
+//submitButton.addEventListener('submit', captureInitals);
+
+function captureInitals(event) {
+    event.preventDefault();
+    console.log(event.target);
+}
 
 
 
